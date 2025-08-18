0c9c8ebc73ce5548abdd3c094c8c8298e0c60499
Francois Pinson <francois.pinson@ovhcloud.com>
2025-08-18 15:14:56 +0000

    Revert "Add log streaming to websocket"
    This reverts commit 3bb07c43a54d7793952100e9decbc500dd8d5f83.

--- internal/progress/progress.go
+++ internal/progress/progress.go
@@ -1,7 +1,6 @@
 package progress
 
 import (
-	"context"
 	"fmt"
 	"net"
 	"os"
@@ -14,12 +13,6 @@ import (
 	"github.com/k0kubun/go-ansi"
 	"github.com/schollz/progressbar/v3"
 	"github.com/vmware/govmomi/vim25/progress"
-
-	"bufio"
-
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
 )
 
 var theme = progressbar.Theme{
@@ -117,56 +110,18 @@ func (w *WebSocketProgressReporter) Percent(percent int, message string) {
 		log.Printf("failed to send WebSocket progress message: %v", err)
 	}
 }
-func (w *WebSocketProgressReporter) StreamLogs() {
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		log.Printf("error loading in-cluster config: %v", err)
-		return
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		log.Printf("error creating kubernetes client: %v", err)
-		return
-	}
-
-	// Rebuild pod name from jobID
-	podName := "migratekit-job-" + w.jobID[:8]
-	namespace := "default"
-
-	req := clientset.CoreV1().Pods(namespace).GetLogs(podName, &corev1.PodLogOptions{
-		Follow: true,
-	})
-
-	stream, err := req.Stream(context.Background())
-	if err != nil {
-		log.Printf("error opening log stream for pod %s: %v", podName, err)
-		return
-	}
-	defer stream.Close()
-
-	scanner := bufio.NewScanner(stream)
-	for scanner.Scan() {
-		line := scanner.Text()
-		msg := ProgressMessage{
-			Type:    "log",
-			Message: line,
-			JobID:   w.jobID,
-		}
-
-		if err := w.conn.WriteJSON(msg); err != nil {
-			log.Printf("error writing log line to websocket: %v", err)
-			return
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		log.Printf("error reading from log stream: %v", err)
-	}
-}
-
 func (w *WebSocketProgressReporter) Close() error {
 	return w.conn.Close()
 }
+
+// func NewVMwareProgressBar(task string) *VMwareProgressBar {
+// 	bar := PercentageProgressBar(task)
+
+// 	return &VMwareProgressBar{
+// 		bar: bar,
+// 		ch:  make(chan progress.Report),
+// 	}
+// }
 
 func NewVMwareProgressBar(jobID string, task string) *VMwareProgressBar {
 	bar := PercentageProgressBar(task)
@@ -198,11 +153,6 @@ func NewDataProgressReporter(desc string, size int64, reporter ProgressReporter,
 			reporter = nil
 		} else {
 			reporter = r
-
-			// Start log streaming if reporter is WebSocket-based
-			if wsReporter, ok := reporter.(*WebSocketProgressReporter); ok {
-				go wsReporter.StreamLogs()
-			}
 		}
 	}
 
